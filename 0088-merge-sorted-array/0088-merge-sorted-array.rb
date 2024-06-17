b6f6aa2ba308c9c808bfec3d4371866dc9d24ee6
dev-stan <67323109+dev-stan@users.noreply.github.com>
2024-06-18 04:18:20 +0900

    Time: 57 ms (70.53%), Space: 211.5 MB (54.68%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+
+   nums1.pop(n)
+   nums1.concat(nums2).sort!
+
+    
+end
